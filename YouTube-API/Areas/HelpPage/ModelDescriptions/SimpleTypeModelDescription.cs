5ce7d4e6dd539aab778fbf5afd56e0adab398c0f
Tom Franklin <tomwilliamfranklin@gmail.com>
2019-03-15 13:05:53 +0000

    revamp
     mostly everything i was doing before was wrong

---
+++ YouTube-API/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace YouTube_API.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ YouTube-API/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="YouTube_API.WebApiApplication" Language="C#" %>
