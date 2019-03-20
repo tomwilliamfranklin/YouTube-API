4d795f2111fdd00a983385e72fc5cbade69c78ca
Tom Franklin <tomwilliamfranklin@gmail.com>
2019-03-20 18:46:47 +0000

    Major Patch
     - Styling Updates
     - Added iframe for video that is clicked on
     - Seach functionality works better now

--- YouTube-API/JavaScript/APIController.js
+++ YouTube-API/JavaScript/APIController.js
@@ -13,28 +13,27 @@ $(document).ready(function () {
     var SearchInput = '';
     var options = {};
     var dataa = 'HELLO';
-    function loadVideos() {
+    function loadVideos() { //takes the URL for api search, options and declares a function with it taking the Data and calling the populateVideos with it
         $.getJSON(URL, options, function (data) {
-            console.log(data);
             populateVideos(data);
         })
     }
-    $("#btnClick").click(function () {
+    $("#btnClick").click(function () { //When search button is clicked, options values are populated, seachinput is set to the input of the search field, and the videos are loaded.
         SearchInput = document.getElementById("txtSearch").value;
-        options = { part: 'snippet', key: key, maxResults: '20', q: SearchInput }
+        options = { part: 'snippet', key: key, maxResults: '5', q: SearchInput } //five videos appear
         loadVideos();
     });
 
 
 
-    function populateVideos(data) {
+    function populateVideos(data) { 
         $('main').html(''); //Clears the html so that previous results are removed
         $.each(data.items, function (i, item) {  //loops through each item, and adds it to the results list 
             var thumb = item.snippet.thumbnails.high.url;
             var Title = item.snippet.title;
             var Description = item.snippet.description.substring(0, 150);
             var id = item.id.videoId;
-            if (Description == "") {
+            if (Description == "") { //if the video doesn't have a description, set description label to "No Description" 
                 Description = "No Description";
             }
 
@@ -45,7 +44,7 @@ $(document).ready(function () {
                     <h4>${Title}</h4>
                     <p>${Description}</p>
                 </div> 
-            </article>
+            </article>  //For each item recieved from the api, a Article is created including the thumbnail, Title and 150 characters of the description
             `);
         });
 
@@ -54,8 +53,8 @@ $(document).ready(function () {
             var getID = $(this).attr('data-key');
             $('#mainvideo').html(`
                 <iframe width="560" height="315" src="https://www.youtube.com/embed/${getID}" frameborder="0" 
-                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            `);
+                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" class="VideoWidget" allowfullscreen></iframe>
+            `); //The video clicked the user appears in the iframe slot. 
             console.log(getID);
         }); 
 
